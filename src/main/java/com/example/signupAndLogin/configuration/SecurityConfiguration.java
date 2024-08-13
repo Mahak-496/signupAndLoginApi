c62467efd5281cad11ca89dd7e5f80f229b348c7
Mahak-496 <mahak.goel@infostride.com>
2024-08-13 11:34:19 +0530

    after adding security dependency

--- src/main/java/com/example/signupAndLogin/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/signupAndLogin/configuration/SecurityConfiguration.java
@@ -1,15 +1,16 @@
-//package com.example.signupAndLogin.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//@Configuration
-//public class SecurityConfiguration {
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//}
+package com.example.signupAndLogin.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration implements WebMvcConfigurer {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
